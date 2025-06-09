8d7f1bdaf77795354ee1e66023d42b93a565eb86
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-09 13:48:55 +0900

    string-method 퀴즈 1. String greeting = "Hello,Alice!"; 2. greeting을 소문자로 출력

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello, Java!";
+        String greeting = "Hello, Alice";
 
-        System.out.println(message.length());
+        System.out.println(greeting.toLowerCase());
+        System.out.println("Length : " + greeting.length());
     }
 }
